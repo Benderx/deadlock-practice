57c40fb9ec4d5878fc140a42708e1a35152e7efd
Andrew Duerig <aqd5396@psu.edu>
2017-04-02 18:05:37 -0400

    Init commit

---
+++ Project2.cpp
@@ -0,0 +1,18 @@
+// project2.cpp
+// By Andrew Duerig
+//
+// how to compile using mingw:
+// g++ .\Project1.cpp myscheduler.cpp -o run.exe -std=c++11
+
+#include <iostream>;
+#include <cstdlib>;
+using namespace std;
+
+
+int main(int argc, char* argv[])
+{
+	cout << "\n--Andrew Duerig's Testbench--\n\n";
+
+	cout << "Testing completed, exiting";
+	return 0;
+}
